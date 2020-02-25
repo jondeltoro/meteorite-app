a28df58b535996b89f7561ebf14d60bf82cf26bc
Jonathan Del Toro <jonathandeltoro@outlook.com>
2020-02-25 03:58:56 -0800

    date picker

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
 import { isEqual } from 'lodash';
 
+import 'react-datepicker/dist/react-datepicker.css';
 import './Controls.scss';
 
 class Controls extends Component {
@@ -20,32 +23,36 @@ class Controls extends Component {
     );
   }
 
+  applyButtonDisabled() {
+    return !this.props.startDate || !this.props.endDate;
+  }
+
   render() {
     return (
       <div className="controls container-fluid">
         <div className="col d-flex align-items-center justify-content-center">
-          <label htmlFor="start-date">Start date</label>
-          <input
-            id="start-date"
-            className="date-element"
-            value={this.props.startDate}
-            onChange={e => this.changeDate('startDate', e.currentTarget.value)}
-          />
+          <label>Start date</label>
+          <DatePicker
+            required
+            maxDate={this.props.endDate}
+            selected={this.props.startDate}
+            onChange={date => this.changeDate('startDate', date)}
+          ></DatePicker>
         </div>
         <div className="col d-flex align-items-center justify-content-center">
           <label htmlFor="end-date">&nbsp;End date&nbsp;</label>
-          <input
-            id="end-date"
-            className="date-element"
-            value={this.props.endDate}
-            onChange={e => this.changeDate('endDate', e.currentTarget.value)}
-          />
+          <DatePicker
+            required
+            minDate={this.props.startDate}
+            selected={this.props.endDate}
+            onChange={date => this.changeDate('endDate', date)}
+          ></DatePicker>
         </div>
         <div className="col-2 d-flex align-items-center justify-content-center">
           <button
             type="button"
             className="btn btn-primary btn-sm btn-query"
-            disabled={this.props.pendingRequest}
+            disabled={this.props.pendingRequest || this.applyButtonDisabled()}
             onClick={_ => this.queryMeteorites()}
           >
             Apply filter
--- src/config.js
+++ src/config.js
@@ -4,13 +4,14 @@ export const maxResults = 10000;
 export const dateFormat = 'MM/DD/YYYY';
 export const apiDateFormat = 'YYYY-MM-DD';
 export const timeStampFormat = 'MM/DD/YYYY HH:mm:ss';
+export const baseUrl = 'https://data.nasa.gov/resource/y77d-th95.json';
 export const generateMeteoritesURL = (startDate, endDate) => {
-  const startYear = moment(startDate, dateFormat).format(apiDateFormat);
-  const endYear = moment(endDate, dateFormat).format(apiDateFormat);
-  return `https://data.nasa.gov/resource/y77d-th95.json?$select=id,name,recclass,mass,year,reclat,reclong&$where=year>=%27${startYear}T00:00:00.000%27%20and%20year<=%27${endYear}T00:00:00.000%27&$limit=${maxResults}`;
+  const startYear = moment(startDate).format(apiDateFormat);
+  const endYear = moment(endDate).format(apiDateFormat);
+  return `${baseUrl}?$select=id,name,recclass,mass,year,reclat,reclong&$where=year>=%27${startYear}T00:00:00.000%27%20and%20year<=%27${endYear}T00:00:00.000%27&$limit=${maxResults}`;
 };
-export const defaultStartDate = '01/01/2010';
-export const defaultEndDate = moment().format(dateFormat);
+export const defaultStartDate = new Date(2010, 0, 1);
+export const defaultEndDate = new Date();
 export const defaultMapProps = {
   center: {
     lat: 35.85,
